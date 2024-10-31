1319f551c42ed108fa196033e25a5c8e5865789b
Mary Farzin <maryam81.farzin@gmail.com>
2024-10-31 13:49:22 +0330

    React Router

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,22 +1,24 @@
-const Pet = ({name, animal, breed, images, location, id}) => {
-    let hero = "http://pets-images.dev-apis.com/pets/none.jpg"
-    if (images.length) {
-        hero = images[0];
-    }
+import { Link } from "react-router-dom";
 
-    return (
-       <a href={`/details/${id}`} className="pet">
-        <div className="images-container">
-            <img src={hero} alt={name}/>
-        </div>
-        <div className="info">
-            <h1>{name}</h1>
-            <h2>
-                {animal} - {breed} - {location}
-            </h2>
-        </div>
-       </a>
-    );
+const Pet = (props) => {
+  const { name, animal, breed, images, location, id } = props;
+
+  let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
+  if (images.length) {
+    hero = images[0];
+  }
+
+  return (
+    <Link to={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} — ${breed} — ${location}`}</h2>
+      </div>
+    </Link>
+  );
 };
 
-  export default Pet;
+export default Pet;
